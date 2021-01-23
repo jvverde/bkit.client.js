e50955ff40919b82b56c853b2b3100fd7d8d3bb1
jvverde at win10 <jvverde@win10>
2021-01-23 18:05:05 +0000

    start confirm

--- cmds/users/create.js
+++ cmds/users/create.js
@@ -1,11 +1,17 @@
 const post = require('../../lib/post')
 const config = require('../../lib/config')
 const { encrypt, decrypt, hmac } = require('../../lib/crypto')
+const stamp = require('../../lib/stamp')
 const inquirer = require('inquirer')
 
+const ask = {
+  code: (email) => inquirer.prompt({ type: 'input', name:'code', message: `Received code on ${email}:` }),
+  pass: () => inquirer.prompt({ type: 'password', name:'pass', message: 'Password:' })
+}
 exports.command = 'create <username> <email>'
 exports.desc = 'Create a new user'
-exports.handler = async ({ username, email }) => {
+exports.handler = async ({ username, email, password }) => {
+  password = password || (await ask.pass()).pass
   try {
     const baseurl = config.load().BKITAPI_BASEURL
     if (!/^https?:\/\//.test(baseurl)) throw new Error(`baseurl=${baseurl}`)
@@ -14,10 +20,17 @@ exports.handler = async ({ username, email }) => {
       email
     })
     console.log('Answer:', answer)
-    const { code } = await inquirer.prompt({ type: 'input', name:'code', message: `Received code on ${email}:` })
+    const { code } = await ask.code(email)
     const secret = decrypt(answer.encsec, code)
-    const digest = hmac(secret, code)
+    if (hmac(secret, code) !== answer.digest) throw new Error(`Wrong code`)
 
+    const { salt, verifier } = await stamp(username, password)
+    const proof = hmac(answer.encsec, code)
+    const encVerifier = encrypt(verifier, secret)
+    const digest = hmac(verifier, secret)
+
+    const confirm = { email, username, salt, encVerifier, proof, username, digest }
+    console.log(confirm)
   } catch (err) {
     console.error('Error:', err)
   }
