8cb12b71d7896d4be4d996ed82a02b673c3021a6
Isidro Vila Verde <jvverde@gmail.com>
2021-02-02 18:03:56 +0000

    join get and post in rest

--- cmds/users/info.js
+++ cmds/users/info.js
@@ -1,4 +1,4 @@
-const get = require('../../lib/get')
+const { getAuth } = require('../../lib/rest')
 const config = require('../../lib/config')
 const inquirer = require('inquirer')
 // const keytar = require('keytar')
@@ -11,12 +11,8 @@ exports.desc = 'Get Current User Info'
 exports.handler = async () => {
   try {
     const conf = config.load()
-    const baseurl = conf.BKITAPI_BASEURL
-    if (!/^https?:\/\//.test(baseurl)) throw new Error(`baseurl=${baseurl}`)
-    const info = await get(`${baseurl}/user/info`, {
-      headers: { Authorization: `Bearer ${conf.TOKEN}` }
-    })
-    console.log(info)
+    const info = await getAuth('/user/info')
+    console.log('info:', info)
   } catch (err) {
     console.error('Error:', err)
   }
