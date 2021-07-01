3ab7a9da2a0b256f96cc1d7ccf5be575c2f6fb8b
Isidro Vila Verde <jvverde@gmail.com>
2021-07-01 15:07:43 +0100

    Long time ago I change this

--- cmds/users/info.js
+++ cmds/users/info.js
@@ -1,10 +1,5 @@
 const { getAuth } = require('../../lib/rest')
-const inquirer = require('inquirer')
-// const keytar = require('keytar')
 
-const ask = {
-  pass: () => inquirer.prompt({ type: 'password', name:'pass', message: 'Password:', mask: '*' })
-}
 exports.command = 'info'
 exports.desc = 'Get Current User Info'
 exports.handler = async () => {
